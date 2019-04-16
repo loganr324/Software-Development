43d0aed37d0e54467eefd811f734468ab21988cc
loganr324 <49657354+loganr324@users.noreply.github.com>
2019-04-15 21:48:49 -0400

    Update LibraryMagazine.cs

--- IComparer Example/Prog1/LibraryMagazine.cs
+++ IComparer Example/Prog1/LibraryMagazine.cs
@@ -1,8 +1,3 @@
-﻿// Program 1A
-// CIS 200-01
-// Due: 2/15/2017
-// By: Andrew L. Wright (Students use Grading ID)
-
 // File: LibraryMagazine.cs
 // This file creates a concrete LibraryMagazine class that adds
 // no new data.
