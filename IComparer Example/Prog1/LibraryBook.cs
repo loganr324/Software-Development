2c7cf68044e1c18f93af47b404b4ffa1cba773f4
loganr324 <49657354+loganr324@users.noreply.github.com>
2019-04-15 21:47:22 -0400

    Update LibraryBook.cs

--- IComparer Example/Prog1/LibraryBook.cs
+++ IComparer Example/Prog1/LibraryBook.cs
@@ -1,8 +1,3 @@
-﻿// Program 1A
-// CIS 200-01
-// Due: 2/15/2017
-// By: Andrew L. Wright (Students use Grading ID)
-
 // File: LibraryBook.cs
 // This file creates a concrete LibraryBook class that adds
 // an author to the LibraryItem data. 
